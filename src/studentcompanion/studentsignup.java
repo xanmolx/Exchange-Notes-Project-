37d78b1d6a75256173b05b12e215dbb11bd080bc
Anmol Singh Sethi <anmolsinghsethi@yahoo.com>
2017-07-04 20:48:40 +0530

    Create studentsignup.java

---
+++ src/studentcompanion/studentsignup.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentcompanion;
+
+/**
+ *
+ * @author anmol singh sethi
+ */
+public class studentdata {
+    String rollno, name, course, fname, phone, email, dob, gender, photo;
+
+    public studentdata(String rollno, String name, String course, String fname, String phone, String email, String dob, String gender/* String password*/, String photo) {
+        this.rollno = rollno;
+        this.name = name;
+        this.course = course;
+        this.fname = fname;
+        this.phone = phone;
+        this.email = email;
+        this.dob = dob;
+        this.gender = gender;
+    //    this.password = password;
+        this.photo = photo;
+    }
+                
+            
+}
