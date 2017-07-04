d5518b6802211c877be11ef5a5ae0c2e55ba7549
Anmol Singh Sethi <anmolsinghsethi@yahoo.com>
2017-07-04 20:49:01 +0530

    Create subjectlist.java

---
+++ src/studentcompanion/subjectlist.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentcompanion;
+
+/**
+ *
+ * @author anmol singh sethi
+ */
+public class subjectlist {
+    String subid;
+    String subname;
+    int sem;
+
+    public subjectlist(String subid, String subname, int sem) {
+        this.subid = subid;
+        this.subname = subname;
+        this.sem = sem;
+    }
+
+    
+}
